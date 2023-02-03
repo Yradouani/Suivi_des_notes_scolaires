33c7ceb9e4067669af5ac2d3b52c041e8d2a346e
Yradouani <yasmine.radouani@outlook.fr>
2023-02-03 09:50:55 +0100

    deconnection and display teacher picture added

--- js/teacher.js
+++ js/teacher.js
@@ -1,4 +1,5 @@
 let studentsCardsContent = document.querySelector("#students-cards-content");
+let deconnectionBtn = document.querySelector("#logoutBtn");
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "../server/students.json", true);
@@ -20,6 +21,38 @@ xhr.onreadystatechange = function () {
             </div>
             `
         }
+
     }
 };
 xhr.send();
+
+// Affichage de la photo du professeur dynamiquement
+var xhr1 = new XMLHttpRequest();
+xhr1.open("GET", "../teachers.json", true);
+xhr1.onreadystatechange = function () {
+    if (xhr1.readyState === 4 && xhr1.status === 200) {
+        var xmlDoc = JSON.parse(xhr1.response);
+
+        let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+        console.log(userInfo);
+        for (let i = 0; i < xmlDoc.length; i++) {
+            if (xmlDoc[i].id == userInfo.id) {
+                document.querySelector("#picAndLogout").innerHTML += `
+                        <img src=${xmlDoc[i].picture} alt="student picture" class="img-fluid rounded-circle">
+                        <button id="logoutBtn">
+                            <i class="bi bi-x-circle-fill text-danger" class="img-fluid"
+                                style="font-size:50px"></i>
+                        </button>
+                        `
+            }
+        }
+
+    }
+};
+xhr1.send();
+
+// --------------Deconnection------------------
+deconnectionBtn.addEventListener("click", () => {
+    localStorage.removeItem('userInfo');
+    window.location.href = "../index.html";
+})
