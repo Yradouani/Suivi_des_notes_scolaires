be41dacf9cb50828fdf1337c89e70e5a56833134
Colas Rabiller <colas.rabiller@gmail.com>
2023-02-06 14:16:35 +0100

    displaying grades ok

--- js/student.js
+++ js/student.js
@@ -2,7 +2,10 @@
 
 let userInfo = JSON.parse(localStorage.getItem("userInfo"));
 let typeUser = userInfo.type;
+
 console.log(typeUser);
+
+// Student datas displayed if user connected is a student
 
 if (typeUser == "student") {
   var xhr = new XMLHttpRequest();
@@ -14,6 +17,7 @@ if (typeUser == "student") {
       console.log(userInfo);
       for (let i = 0; i < xmlDoc.length; i++) {
         if (xmlDoc[i].id == userInfo.id) {
+          // displaying name and picture of the student
           document.querySelector("#picAndLogout").innerHTML += `
                         <img src=${xmlDoc[i].picture} alt="student picture" class="img-fluid rounded-circle">
                         <div id="name-content">${xmlDoc[i].firstname} ${xmlDoc[i].lastname}</div>`;
@@ -22,25 +26,121 @@ if (typeUser == "student") {
     }
   };
   xhr.send();
-} else if (typeUser == "teacher") {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "./teacher.json", true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var xmlDoc = JSON.parse(xhr.response);
+  let idStudent = userInfo.id;
+  console.log(idStudent);
 
-      console.log(userInfo);
-      for (let i = 0; i < xmlDoc.length; i++) {
-        if (xmlDoc[i].id == userInfo.id) {
-          document.querySelector("#picAndLogout").innerHTML += `
-                        <img src=${xmlDoc[i].picture} alt="student picture" class="img-fluid rounded-circle">
-                        <div id="name-content">${xmlDoc[i].firstname} ${xmlDoc[i].lastname}</div>`;
+  // Displaying school report
+  let studentReport = document.querySelector("#studentReport");
+  var xhr1 = new XMLHttpRequest();
+
+  xhr1.open("GET", "./server/grades.json", true);
+  xhr1.onreadystatechange = function () {
+    if (xhr1.readyState === 4 && xhr1.status === 200) {
+      var xmlDoc1 = JSON.parse(xhr1.response);
+      // console.log(xmlDoc1);
+
+      for (let i = 0; i < xmlDoc1.length; i++) {
+        // console.log(xmlDoc1[i].value);
+        if (xmlDoc1[i].id_student == idStudent) {
+          if (i % 2 == 0) {
+            studentReport.innerHTML += `
+      <tr class="pinkLine">
+      <th scope="row">${xmlDoc1[i].subject}</th>
+      <td class="type_eval_coef">${xmlDoc1[i].type}</td>
+      <td class="average">(moyenne à calculer)</td>
+      <td class="marks">${xmlDoc1[i].value} "$nbsp"</td>
+      <td class="graph_link"> <a href=""><img src="./assets/stats.jpg" alt="graph_link"
+                  width="70"></a></td>
+  </tr>
+          `;
+          } else {
+            studentReport.innerHTML += `
+          <tr class="whiteLine">
+          <th scope="row">${xmlDoc1[i].subject}</th>
+          <td class="type_eval_coef">${xmlDoc1[i].type}</td>
+          <td class="average">(moyenne à calculer)</td>
+          <td class="marks">${xmlDoc1[i].value}</td>
+          <td class="graph_link"> <a href=""><img src="./assets/stats.jpg" alt="graph_link"
+                      width="70"></a></td>
+      </tr>
+              `;
+          }
         }
       }
     }
   };
-  xhr.send();
+  xhr1.send();
+
+  // Displaying student datas if teacher is connected
+} else if (typeUser == "teacher") {
+  var xhr2 = new XMLHttpRequest();
+  xhr2.open("GET", "../teachers.json", true);
+  xhr2.onreadystatechange = function () {
+    if (xhr2.readyState === 4 && xhr2.status === 200) {
+      var xmlDoc2 = JSON.parse(xhr2.response);
+      console.log(xmlDoc2);
+
+      let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+      console.log(userInfo);
+      for (let i = 0; i < xmlDoc2.length; i++) {
+        if (xmlDoc2[i].id == userInfo.id) {
+          // displaying teacher picture and name on header
+          document.querySelector("#picAndLogout").innerHTML += `
+                          <img src=${xmlDoc2[i].picture} alt="student picture" class="img-fluid rounded-circle">
+                          <div id="name-content">${xmlDoc2[i].firstname} ${xmlDoc2[i].lastname}</div>
+                          `;
+        }
+      }
+    }
+  };
+  xhr2.send();
+
+  // Displaying school report of the student
+  let studentReport = document.querySelector("#studentReport");
+  var xhr3 = new XMLHttpRequest();
+  const urlParams = new URLSearchParams(window.location.search);
+  const paramValue = urlParams.get("id");
+  console.log(paramValue);
+
+  xhr3.open("GET", "../server/grades.json", true);
+  xhr3.onreadystatechange = function () {
+    if (xhr3.readyState === 4 && xhr3.status === 200) {
+      var xmlDoc3 = JSON.parse(xhr3.response);
+      console.log(xmlDoc3);
+
+      for (let i = 0; i < xmlDoc3.length; i++) {
+        // console.log(JSON.stringify(xmlDoc1[i].id_student));
+        if (JSON.stringify(xmlDoc3[i].id_student) == paramValue) {
+          console.log("coucou");
+          if (i % 2 == 0) {
+            studentReport.innerHTML += `
+              <tr class="pinkLine">
+              <th scope="row">${xmlDoc3[i].subject}</th>
+              <td class="type_eval_coef">${xmlDoc3[i].type}</td>
+              <td class="average">(moyenne à calculer)</td>
+              <td class="marks">${xmlDoc3[i].value}</td>
+              <td class="graph_link"> <a href=""><img src="./assets/stats.jpg" alt="graph_link"
+                          width="70"></a></td>
+          </tr>
+                  `;
+          } else {
+            studentReport.innerHTML += `
+                  <tr class="whiteLine">
+                  <th scope="row">${xmlDoc3[i].subject}</th>
+                  <td class="type_eval_coef">${xmlDoc3[i].type}</td>
+                  <td class="average">(moyenne à calculer)</td>
+                  <td class="marks">${xmlDoc3[i].value}</td>
+                  <td class="graph_link"> <a href=""><img src="./assets/stats.jpg" alt="graph_link"
+                              width="70"></a></td>
+              </tr>
+                      `;
+          }
+        }
+      }
+    }
+  };
 }
+xhr3.send();
 
 // --------------Logout------------------
 let deconnectionBtn = document.querySelector("#logoutBtn");
@@ -49,31 +149,3 @@ deconnectionBtn.addEventListener("click", () => {
   localStorage.removeItem("userInfo");
   window.location.href = "../index.html";
 });
-
-// ----------Display grades ------------------
-
-let studentMarks = document.querySelector(".marks");
-
-// var xhr1 = new XMLHttpRequest();
-// xhr1.open("GET", "../server/students.json", true);
-// xhr1.onreadystatechange = function () {
-//   if (xhr1.readyState === 4 && xhr.status === 200) {
-//     var xmlDoc = JSON.parse(xhr.response);
-//     console.log(xmlDoc);
-
-//     for (let i = 0; i < xmlDoc.length; i++) {
-//       console.log(xmlDoc[i].value);
-//       studentMarks.innerHTML += `
-//             <div class="cards">
-//                 <img src=${xmlDoc[i].picture} alt="">
-//                 <span class="name">${xmlDoc[i].firstname} ${xmlDoc[i].lastname}</span>
-//                 <div class="btn-container">
-//                     <button class="add-btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Ajouter une note</button>
-//                     <button class="grade-btn"><a href="../student.html">Voir le bulletin</a></button>
-//                 </div>
-//             </div>
-//             `;
-//     }
-//   }
-// };
-// xhr1.send();
--- js/teacher.js
+++ js/teacher.js
@@ -4,69 +4,71 @@ let deconnectionBtn = document.querySelector("#logoutBtn");
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "../server/students.json", true);
 xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        var xmlDoc = JSON.parse(xhr.response);
-        console.log(xmlDoc);
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    var xmlDoc = JSON.parse(xhr.response);
+    console.log(xmlDoc);
 
-        for (let i = 0; i < xmlDoc.length; i++) {
-            console.log(xmlDoc[i].picture)
-            studentsCardsContent.innerHTML += `
+    for (let i = 0; i < xmlDoc.length; i++) {
+      console.log(xmlDoc[i].picture);
+      studentsCardsContent.innerHTML += `
             <div class="cards">
                 <img src=${xmlDoc[i].picture} alt="">
                 <span class="name">${xmlDoc[i].firstname} ${xmlDoc[i].lastname}</span>
                 <div class="btn-container">
                     <button class="add-btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Ajouter une note</button>
                     <button class="grade-btn">
-                    <a href="../student.html?{${xmlDoc[i].id}}">
+                    <a href="../student.html?id=${xmlDoc[i].id}">
                     Voir le bulletin
                     </a>
                     </button>
                 </div>
             </div>
-            `
-        }
-
-        // Ajouter une note
-        let addGradeButtons = document.querySelectorAll(".add-btn");
-        console.log(addGradeButtons)
-        for (let j = 0; j < addGradeButtons.length; j++) {
-            console.log(addGradeButtons[j])
-            addGradeButtons[j].addEventListener("click", () => {
-                let validateGradeBtn = document.querySelector(".add-grade-btn");
-                validateGradeBtn.addEventListener("click", () => {
-                    let type = document.querySelector("#eval-choice");
-                    let dateInput = document.querySelector("#date");
-                    let gradeInput = document.querySelector("#grade");
-                    let commentInput = document.querySelector("#comment");
-
-
-                    var xhr2 = new XMLHttpRequest();
-                    xhr2.open("GET", "../server/grades.json", true);
-                    xhr2.onreadystatechange = function () {
-                        if (xhr2.readyState === 4 && xhr2.status === 200) {
-                            var xmlGrade = JSON.parse(xhr2.response);
-                            var gradesNumber = xmlGrade.length;
-
-                            let gradeInfo = {
-                                "id": gradesNumber + 1,
-                                "value": gradeInput.value,
-                                "date": dateInput.value,
-                                "type": type.value,
-                                "id_student": j + 1,
-                                "subject": "Histoire",
-                                "comments": commentInput.value
-                            }
-                            console.log(gradeInfo)
-                            console.log(type.value, dateInput.value, gradeInput.value, commentInput.value)
-                        }
-                    }
-                    xhr2.send();
-
-                })
-            })
-        }
-
+            `;
     }
+
+    // Ajouter une note
+    let addGradeButtons = document.querySelectorAll(".add-btn");
+    console.log(addGradeButtons);
+    for (let j = 0; j < addGradeButtons.length; j++) {
+      console.log(addGradeButtons[j]);
+      addGradeButtons[j].addEventListener("click", () => {
+        let validateGradeBtn = document.querySelector(".add-grade-btn");
+        validateGradeBtn.addEventListener("click", () => {
+          let type = document.querySelector("#eval-choice");
+          let dateInput = document.querySelector("#date");
+          let gradeInput = document.querySelector("#grade");
+          let commentInput = document.querySelector("#comment");
+
+          var xhr2 = new XMLHttpRequest();
+          xhr2.open("GET", "../server/grades.json", true);
+          xhr2.onreadystatechange = function () {
+            if (xhr2.readyState === 4 && xhr2.status === 200) {
+              var xmlGrade = JSON.parse(xhr2.response);
+              var gradesNumber = xmlGrade.length;
+
+              let gradeInfo = {
+                id: gradesNumber + 1,
+                value: gradeInput.value,
+                date: dateInput.value,
+                type: type.value,
+                id_student: j + 1,
+                subject: "Histoire",
+                comments: commentInput.value,
+              };
+              console.log(gradeInfo);
+              console.log(
+                type.value,
+                dateInput.value,
+                gradeInput.value,
+                commentInput.value
+              );
+            }
+          };
+          xhr2.send();
+        });
+      });
+    }
+  }
 };
 xhr.send();
 
@@ -74,54 +76,59 @@ xhr.send();
 var xhr1 = new XMLHttpRequest();
 xhr1.open("GET", "../teachers.json", true);
 xhr1.onreadystatechange = function () {
-    if (xhr1.readyState === 4 && xhr1.status === 200) {
-        var xmlDoc = JSON.parse(xhr1.response);
+  if (xhr1.readyState === 4 && xhr1.status === 200) {
+    var xmlDoc = JSON.parse(xhr1.response);
 
-        let userInfo = JSON.parse(localStorage.getItem("userInfo"));
-        console.log(userInfo);
-        for (let i = 0; i < xmlDoc.length; i++) {
-            if (xmlDoc[i].id == userInfo.id) {
-                document.querySelector("#picAndLogout").innerHTML += `
+    let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    console.log(userInfo);
+    for (let i = 0; i < xmlDoc.length; i++) {
+      if (xmlDoc[i].id == userInfo.id) {
+        document.querySelector("#picAndLogout").innerHTML += `
                         <img src=${xmlDoc[i].picture} alt="student picture" class="img-fluid rounded-circle">
                         <div id="name-content">${xmlDoc[i].firstname} ${xmlDoc[i].lastname}</div>
-                        `
-            }
-        }
+                        `;
+      }
     }
+  }
 };
 xhr1.send();
 
 // --------------Deconnection------------------
 deconnectionBtn.addEventListener("click", () => {
-    localStorage.removeItem('userInfo');
-    window.location.href = "../index.html";
-})
-// 
+  localStorage.removeItem("userInfo");
+  window.location.href = "../index.html";
+});
+//
 // Ajouter une note
 let addGradeButtons = document.querySelectorAll(".add-btn");
-console.log(addGradeButtons)
+console.log(addGradeButtons);
 for (let j = 0; j < addGradeButtons.length; j++) {
-    console.log(addGradeButtons[j])
-    addGradeButtons[j].addEventListener("click", () => {
-        console.log("coucou")
-        let validateGradeBtn = document.querySelector(".add-grade-btn");
-        validateGradeBtn.addEventListener("click", () => {
-            console.log("coucou")
-            let type = document.querySelector("#eval-choice");
-            let dateInput = document.querySelector("#date");
-            let gradeInput = document.querySelector("#grade");
-            let commentInput = document.querySelector("#comment");
+  console.log(addGradeButtons[j]);
+  addGradeButtons[j].addEventListener("click", () => {
+    console.log("coucou");
+    let validateGradeBtn = document.querySelector(".add-grade-btn");
+    validateGradeBtn.addEventListener("click", () => {
+      console.log("coucou");
+      let type = document.querySelector("#eval-choice");
+      let dateInput = document.querySelector("#date");
+      let gradeInput = document.querySelector("#grade");
+      let commentInput = document.querySelector("#comment");
 
-            console.log(type.value, dateInput.value, gradeInput.value, commentInput.value)
-        })
-    })
+      console.log(
+        type.value,
+        dateInput.value,
+        gradeInput.value,
+        commentInput.value
+      );
+    });
+  });
 }
 
 // Voir le bulletin de l'élève
 let watchGradeButtons = document.querySelectorAll(".grade-btn");
 
 for (let j = 0; j < watchGradeButtons.length; j++) {
-    watchGradeButtons[j].addEventListener("click", () => {
-        console.log("coucou")
-    })
+  watchGradeButtons[j].addEventListener("click", () => {
+    console.log("coucou");
+  });
 }
