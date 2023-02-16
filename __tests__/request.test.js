1c7d2abcb0455982d99affeddfa24ca905325990
Yradouani <yasmine.radouani@outlook.fr>
2023-02-16 16:25:17 +0100

    integration test

--- __tests__/request.test.js
+++ __tests__/request.test.js
@@ -1,20 +1,18 @@
 import 'isomorphic-fetch';
+import fs from 'fs';
+import path from 'path';
 
 test('real fetch call', async () => {
-    const res = await fetch("http://127.0.0.1:8000/json.php", {
-        method: "GET",
-        headers: {
-            "Content-type":
-                "application/x-www-form-urlencoded; charset=UTF-8",
-        }
-    });
-    const result = await res.json();
-    expect(result[0]).toHaveProperty('id');
-    expect(result[0]).toHaveProperty('value');
-    expect(result[0]).toHaveProperty('date');
-    expect(result[0]).toHaveProperty('type');
-    expect(result[0]).toHaveProperty('coef');
-    expect(result[0]).toHaveProperty('id_student');
-    expect(result[0]).toHaveProperty('subject');
-    expect(result[0]).toHaveProperty('comments');
+  const filePath = path.resolve(__dirname, '../server/grades.json');
+  const fileContents = await fs.promises.readFile(filePath, 'utf8');
+  const data = JSON.parse(fileContents);
+
+  expect(data[0]).toHaveProperty('id');
+  expect(data[0]).toHaveProperty('value');
+  expect(data[0]).toHaveProperty('date');
+  expect(data[0]).toHaveProperty('type');
+  expect(data[0]).toHaveProperty('coef');
+  expect(data[0]).toHaveProperty('id_student');
+  expect(data[0]).toHaveProperty('subject');
+  expect(data[0]).toHaveProperty('comments');
 });
